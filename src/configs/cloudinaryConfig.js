8c8548c0d18009d715f48bef01a8c82dab3b108f
Phat Tran <phat.trancompsci@hcmut.edu.vn>
2025-11-05 12:58:06 +0700

    add log cloudinary

--- src/configs/cloudinaryConfig.js
+++ src/configs/cloudinaryConfig.js
@@ -3,10 +3,15 @@ const envConfig = require('./envConfig')
 
 const { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME } =
     envConfig
-cloudinary.config({
-    cloud_name: CLOUDINARY_CLOUD_NAME,
-    api_key: CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET,
-})
+try {
+    cloudinary.config({
+        cloud_name: CLOUDINARY_CLOUD_NAME,
+        api_key: CLOUDINARY_API_KEY,
+        api_secret: CLOUDINARY_API_SECRET,
+    })
+    console.log('Cloudinary connected')
+} catch (error) {
+    console.log(error)
+}
 
 module.exports = cloudinary
